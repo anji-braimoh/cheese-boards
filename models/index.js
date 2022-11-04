beb41f014799099e9f7d58d7278b73e63f3ebb2b
anji-braimoh <anjolabraimoh@outlook.com>
2022-11-04 11:10:08 +0000

    added two associations

--- models/index.js
+++ models/index.js
@@ -2,6 +2,14 @@ const Board = require ('./board.model')
 const Cheese =  require ('./cheese.model')
 const User = require ('./user.model')
 
+User.hasMany(Board)
+Board.belongsTo(User)
+
+
+Board.belongsToMany(Cheese,{through:'Board_Cheese'})
+Cheese.belongsToMany(Board,{through:'Board_Cheese'})
+
+
 module.exports ={
     Board,
     Cheese,
