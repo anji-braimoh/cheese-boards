612e93510af4a1760ff0182d53e240446ae2b253
anji-braimoh <anjolabraimoh@outlook.com>
2022-11-04 15:44:47 +0000

    added some testing to user test file

---
+++ tests/user.test.js
@@ -0,0 +1,14 @@
+const User = require("../models/user.model");
+const db = require("../db/db");
+const {seed} = require("../db/seed");
+const { describe } = require("../models/user.model");
+
+
+beforeAll (async () => await seed ())
+
+describe("Does the table work" , () => {
+     test ('User table is made' , async() => {
+        const data = await db.getQueryInterface().showAllSchemas();
+        expect(data[0].name).toBe('Users');
+     })
+})
